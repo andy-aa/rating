523075115cc163125df31a962298ff94c80e5862
andy-aa <46691193+andy-aa@users.noreply.github.com>
2019-03-23 20:21:51 +0300

    Initial commit

---
+++ js/stars.js
@@ -0,0 +1,33 @@
+document.querySelectorAll(".rating").forEach((element) => {
+
+    let product_id = element.getAttribute("data-product-id");
+
+    element.addEventListener("click", e => {
+        let rat_val = e.target.getAttribute("data-rat-val");
+        let storedItem = localStorage.getItem("arrRat");
+        let arrRat = storedItem ? JSON.parse(storedItem) : {};
+
+        if (product_id && rat_val) {
+            arrRat[product_id] = rat_val
+        }
+
+        localStorage.setItem("arrRat", JSON.stringify(arrRat));
+
+        dravStars(element);
+    });
+
+    dravStars(element);
+});
+
+function dravStars(element) {
+    let star_count = JSON.parse(localStorage.getItem("arrRat"))[element.getAttribute("data-product-id")];
+
+    element.querySelectorAll("div").forEach(element => {
+        let current_rate = element.getAttribute("data-rat-val");
+        if ((current_rate > 0) && (current_rate <= star_count)) {
+            element.classList.add("star");
+        } else {
+            element.classList.remove("star");
+        }
+    });
+}
